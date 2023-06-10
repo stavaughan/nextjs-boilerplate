60132ae9af1118a501624d2f26f03665e5534255
Stephen Vaughan <stavaughan@users.noreply.github.com>
2023-06-10 14:26:49 -0500

    completed conversion to typescript

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import React from 'react';
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
